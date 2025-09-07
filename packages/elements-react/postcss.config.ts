29a1d331bc4508be27cd4e238d3974bac2c91614
seungyeop-lee <xbeast7@gmail.com>
2025-09-07 16:47:03 +0900

    fix: elements-react build 에러 수정

--- packages/elements-react/postcss.config.ts
+++
@@ -1,7 +0,0 @@
-// Copyright © 2024 Ory Corp
-// SPDX-License-Identifier: Apache-2.0
-
-module.exports = {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-require-imports
-  plugins: [require("@tailwindcss/postcss")()],
-}
